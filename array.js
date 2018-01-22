60c03fdffed74d239afc139dfa4c6594ebf00e02
Benjamin Bowes <bbowes999@gmail.com>
2018-01-22 14:47:06 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -13,7 +13,8 @@ return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+ array.push(element)
+  return array
 }
 
 function accessElementInArray(array, index) {
